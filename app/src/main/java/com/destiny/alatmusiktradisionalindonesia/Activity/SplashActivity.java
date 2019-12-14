3230f6086f6fc01b41a46aff2bc6112ad0617c25
Muhammad Supriyadi <musupadi159@gmail.com>
2019-12-14 22:32:11 +0700

    DONE

--- app/src/main/java/com/destiny/alatmusiktradisionalindonesia/Activity/SplashActivity.java
+++ app/src/main/java/com/destiny/alatmusiktradisionalindonesia/Activity/SplashActivity.java
@@ -2,20 +2,23 @@ package com.destiny.alatmusiktradisionalindonesia.Activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import com.destiny.alatmusiktradisionalindonesia.R;
 
 public class SplashActivity extends Activity {
-
+    MediaPlayer SuaraMe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        SuaraMe = MediaPlayer.create(SplashActivity.this,R.raw.pembuka);
+        SuaraMe.start();
         Thread thread = new Thread() {
             public void run() {
                 try {
-                    sleep(3000);
+                    sleep(5000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } finally {
